ba5a01c1bb999750d8761c3d6fd43079e94b0102
MitchellWT <mitchell444999@gmail.com>
2022-01-05 22:27:59 +1100

    Basic reading from file system and saving to json has been implemented, requires refactoring and seperation

--- main.go
+++ main.go
@@ -1,32 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
-type scanFile struct {
-	path string
-	size int64
+// ScanFile is the singular file scanned from the application
+type ScanFile struct {
+	Path string
+	Size int64
 }
 
-// TODO: get file tranfersal to run in constant time
-// TODO: ignore certain linux folders such as /proc
-func getAllFiles(rootFile string, allFiles []scanFile) []scanFile {
-	files, err := ioutil.ReadDir(rootFile)
+// ScanData is the data that will be stored in json form in the /var/lib/gscan/data dir
+type ScanData struct {
+	DateTime  string
+	RootDir   string
+	ScanFiles []ScanFile
+}
+
+var linuxIgnore []string
+
+func ignoreSearch(dirName string, ignoreArr []string) bool {
+	for _, ignoreDir := range ignoreArr {
+		if ignoreDir == dirName {
+			return true
+		}
+	}
+	return false
+}
+
+func errorCheck(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getAllFiles(rootFile string, allFiles []ScanFile) []ScanFile {
+	files, err := ioutil.ReadDir(rootFile)
+	errorCheck(err)
 
 	for _, file := range files {
 		if file.IsDir() {
+			if ignoreSearch(file.Name(), linuxIgnore) {
+				continue
+			}
 			allFiles = getAllFiles(rootFile+"/"+file.Name(), allFiles)
 		} else {
-			allFiles = append(allFiles, scanFile{
-				rootFile + "/" + file.Name(),
-				file.Size(),
+			if file.Mode()&os.ModeSymlink == os.ModeSymlink {
+				continue
+			}
+			allFiles = append(allFiles, ScanFile{
+				Path: rootFile + "/" + file.Name(),
+				Size: file.Size(),
 			})
 		}
 	}
@@ -43,9 +73,32 @@ func getRuntimeArgs() []string {
 	return returnArgs
 }
 
+func saveToFile(rootFile string, allFiles []ScanFile) {
+	currentTime := time.Now().Format(time.UnixDate)
+	dataFolder := strings.ReplaceAll(rootFile, "/", "_")
+	dataDir := "/var/lib/gscan/data/"
+	fileName := dataDir + dataFolder + " " + currentTime + ".json"
+	jsonData := ScanData{
+		DateTime:  currentTime,
+		RootDir:   rootFile,
+		ScanFiles: allFiles,
+	}
+	jsonBytes, err := json.Marshal(jsonData)
+	errorCheck(err)
+	err = os.MkdirAll(dataDir, 0755)
+	errorCheck(err)
+	fmt.Println(string(jsonBytes))
+	err = os.WriteFile(fileName, jsonBytes, 0766)
+	errorCheck(err)
+}
+
+func init() {
+	linuxIgnore = append(linuxIgnore, "proc", "run", "dev", "sys")
+}
+
 func main() {
 	rootFile := getRuntimeArgs()[0]
-	allFiles := make([]scanFile, 0)
+	allFiles := make([]ScanFile, 0)
 	allFiles = getAllFiles(rootFile, allFiles)
-	fmt.Println(allFiles)
+	saveToFile(rootFile, allFiles)
 }
