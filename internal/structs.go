0842d884baa771bf62d4a91443dff78fbdedd5ec
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-01-12 18:20:15 +1100

    Added missing structs for export funct

--- internal/structs.go
+++ internal/structs.go
@@ -12,3 +12,21 @@ type ScanData struct {
 	RootDir   string
 	ScanFiles []ScanFile
 }
+
+// ExportRaw stores the information that will be exported when a command
+// is called
+type ExportRaw struct {
+	UnixStartTime int64
+	UnixEndTime   int64
+	RootDir       string
+	ScanFiles     map[int64][]ScanFile
+}
+
+// ExportCollectedRaw stores the information that will be exported when
+// a command is called
+type ExportCollectedRaw struct {
+	UnixStartTime int64
+	UnixEndTime   int64
+	RootDir       string
+	TotalDiff     map[int64]int64
+}
