d947c27eb98b1e88cfba8ec7f4676d1aa139d228
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-02-09 18:14:41 +1100

    Added commenting to html export structs

--- internal/structs/html_template_data.go
+++ internal/structs/html_template_data.go
@@ -1,5 +1,6 @@
 package structs
 
+// HTMLTemplateData stores the information used in HTML exporting
 type HTMLTemplateData struct {
 	Title       string
 	GraphLabels []string
--- internal/structs/html_template_data_set.go
+++ internal/structs/html_template_data_set.go
@@ -1,5 +1,7 @@
 package structs
 
+// HTMLTemplateDataSet stores one data set for the HTML export. This data
+// directly maps to the data required for Chart.js
 type HTMLTemplateDataSet struct {
 	Label      string
 	LineColour string
